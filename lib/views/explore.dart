72c5a0382d2bcadbe487bc70815f118c2d1155af
MHusnainShahid <79741701+muhammadhusnainshahid@users.noreply.github.com>
2025-10-04 15:16:16 +0500

    Update explore.dart

--- lib/views/explore.dart
+++ lib/views/explore.dart
@@ -1,8 +1,6 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:art_gallery/const/appExtension.dart';
 import 'package:art_gallery/const/const.dart';
-import 'package:art_gallery/model/productmodel.dart';
-import 'package:art_gallery/views/detail_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
@@ -14,28 +12,6 @@ class ExploreView extends StatefulWidget {
   State<ExploreView> createState() => _ExploreViewState();
 }
 
-class _ExploreViewState extends State<ExploreView> {
-  int? hoveredIndex;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: SizedBox(
-          height: 50,
-          child: TextField(
-            decoration: InputDecoration(
-              hintText: 'Search',
-              prefixIcon: Icon(Icons.search),
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(12),
-                borderSide: BorderSide.none,
-              ),
-              filled: true,
-              fillColor: Colors.grey[300],
-            ),
-          ),
-        ),
-      ),
       body: Column(
         children: [
           10.vspace,
@@ -91,27 +67,7 @@ class _ExploreViewState extends State<ExploreView> {
             color: Colors.white,
             borderRadius: BorderRadius.circular(16),
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // Profile Pic and Name
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 20,
-                      backgroundImage: NetworkImage(profile),
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      'Mike Zungiya',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.w600,
-                      ),
-                    ),
+        
                     Spacer(),
                     IconButton(
                       onPressed: () => Navigator.pop(context),
@@ -132,15 +88,6 @@ class _ExploreViewState extends State<ExploreView> {
                 padding: const EdgeInsets.symmetric(
                   horizontal: 16,
                   vertical: 12,
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(Icons.favorite_border_outlined, color: Colors.black),
-                    Icon(Icons.person_add_alt_1_outlined, color: Colors.black),
-                    Icon(Icons.send_outlined, color: Colors.black),
-
-                    Icon(Icons.bookmark_border, color: Colors.black),
                   ],
                 ),
               ),
